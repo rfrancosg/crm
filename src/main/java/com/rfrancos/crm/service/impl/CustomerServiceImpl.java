970817367fdbc55df91f3c9701addce3fe5890f6
Raul Francos <raul.francos@concatel.com>
2024-11-02 11:29:38 +0100

    exceptions

--- src/main/java/com/rfrancos/crm/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/rfrancos/crm/service/impl/CustomerServiceImpl.java
@@ -3,14 +3,11 @@ package com.rfrancos.crm.service.impl;
 import com.rfrancos.crm.dto.create.CreateCustomerDto;
 import com.rfrancos.crm.dto.get.GetCustomerDto;
 import com.rfrancos.crm.dto.update.UpdateCustomerDto;
-import com.rfrancos.crm.exceptions.CustomException;
+import com.rfrancos.crm.exceptions.NotFoundException;
 import com.rfrancos.crm.mapper.CustomerMapper;
 import com.rfrancos.crm.repository.CustomerRepository;
 import com.rfrancos.crm.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,27 +29,17 @@ public class CustomerServiceImpl implements CustomerService {
     public GetCustomerDto getCustomerById(Long id) {
         return customerRepository.findById(id)
                 .map(customerMapper::toGetCustomerDto)
-                .orElseThrow(() -> new CustomException("Customer not found for id: " + id, HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException("Customer not found for id: " + id));
     }
 
     @Override
     public GetCustomerDto createCustomer(CreateCustomerDto createCustomerDto) {
-        try {
-            return customerMapper.toGetCustomerDto(customerRepository.save(customerMapper.createCustomerDtoToEntity(createCustomerDto)));
-        }
-        catch (DataIntegrityViolationException exception) {
-            throw new CustomException(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return customerMapper.toGetCustomerDto(customerRepository.save(customerMapper.createCustomerDtoToEntity(createCustomerDto)));
     }
 
     @Override
     public GetCustomerDto updateCustomer(UpdateCustomerDto updateCustomerDto) {
-        try {
-            return customerMapper.toGetCustomerDto(customerRepository.save(customerMapper.updateCustomerDtoToEntity(updateCustomerDto)));
-        }
-        catch (DataIntegrityViolationException exception) {
-            throw new CustomException(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return customerMapper.toGetCustomerDto(customerRepository.save(customerMapper.updateCustomerDtoToEntity(updateCustomerDto)));
     }
 
     @Override
